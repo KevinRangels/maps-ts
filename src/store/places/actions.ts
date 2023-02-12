c886315dfe4be527e501ca2f3c8624452351b2df
Kevin Rangel <krangel107@gmail.com>
2023-02-12 09:00:20 -0500

    Create usePlacesStore

--- src/store/places/actions.ts
+++ src/store/places/actions.ts
@@ -4,8 +4,14 @@ import { StateInterface } from '../index';
 
 
 const actions: ActionTree<PlacesState, StateInterface> = {
-    someAction( /*{ commit }, payload  */ ) {
-        // a line to prevent linter errors
+    getInitialLocation( { commit } ) {
+        navigator.geolocation.getCurrentPosition(
+            ({coords}) => commit('setLngLat', {lng: coords.longitude, lat: coords.latitude}),
+            (err) => {
+                console.error(err)
+                throw new Error('No geolocation')
+            }
+        )
     }
 }
 
